949648a40c9839ef946e774146ff1d7480acbe00
gvdp <vdputteglenn@gmail.com>
2024-10-12 15:26:09 +0200

    chore: move to nextjs app router

---
+++ app/layout.tsx
@@ -0,0 +1,43 @@
+import { Metadata } from 'next'
+import '../styles/globals.css'
+import Head from 'next/head'
+
+export const metadata: Metadata = {
+  title: 'Glenn Van De Putte',
+  description: 'Personal website for Glenn Van De Putte.',
+  icons: 'logo.svg',
+  verification: { google: 'Pj1ATvoL2tXCTzxzxr9aG_u73AjTkwjFbXsmm0P9hPw' },
+  // todo: add properties for og meta headers
+}
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  const GA_MEASUREMENT_ID = 'G-7WKLQH1LCM'
+
+  return (
+    <html lang="en">
+      <Head>
+        {process.env.DISABLE_ANALYTICS !== 'true' ? (
+          <>
+            <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`} />
+            <script
+              // eslint-disable-next-line react/no-danger
+              dangerouslySetInnerHTML={{
+                __html: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', '${GA_MEASUREMENT_ID}', {
+          page_path: window.location.pathname,
+        });
+      `,
+              }}
+            />
+          </>
+        ) : (
+          <></>
+        )}
+      </Head>
+      <body>{children}</body>
+    </html>
+  )
+}
