f1ea2ec2e88dacdb37fecc5a4d18b424186ab026
gvdp <vdputteglenn@gmail.com>
2025-06-09 14:51:35 +0200

    ci: add plugin for rewriting base url

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,5 +1,9 @@
+import { HtmlBasePlugin } from "@11ty/eleventy";
+
+
 export default async function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('switch-theme.js')
+	eleventyConfig.addPlugin(HtmlBasePlugin);
 
   return {
     dir: {
